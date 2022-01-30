6a04e9c73c87f0aa8a9c7f8a7fc5e578879582dd
Thomas Drevon <thomas@drevon.no>
2022-01-30 17:58:41 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,62 @@
+function stop () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    radio.sendValue("stop", 1)
+}
+input.onButtonPressed(Button.A, function () {
+    running = 1
+    start()
+})
+input.onButtonPressed(Button.B, function () {
+    running = 0
+    stop()
+})
+function start () {
+    while (running == 1) {
+        music.playTone(262, music.beat(BeatFraction.Sixteenth))
+        forward = input.acceleration(Dimension.Y) * -1
+        turn = input.acceleration(Dimension.X)
+        updateStatus()
+        radio.sendValue("forward", forward)
+        radio.sendValue("turn", turn)
+    }
+}
+function updateStatus () {
+    basic.clearScreen()
+    led.plot(2, 2)
+    if (forward < -700) {
+        led.plot(2, 4)
+    }
+    if (forward < -200) {
+        led.plot(2, 3)
+    }
+    if (forward > 200) {
+        led.plot(2, 1)
+    }
+    if (forward > 700) {
+        led.plot(2, 0)
+    }
+    if (turn < -700) {
+        led.plot(0, 2)
+    }
+    if (turn < -200) {
+        led.plot(1, 2)
+    }
+    if (turn > 200) {
+        led.plot(3, 2)
+    }
+    if (turn > 700) {
+        led.plot(4, 2)
+    }
+}
+let turn = 0
+let forward = 0
+let running = 0
+radio.setGroup(1)
+let threshold = 200
+music.setVolume(36)
